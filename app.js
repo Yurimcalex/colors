b4f3ee1cdbfff5d14f000dfa33f0c6d72b4e5624
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-18 17:02:21 +0200

    Set random card colors on space click

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const cols = document.querySelectorAll('.col');
 
+document.addEventListener('keydown', (event) => {
+	if (event.code.toLowerCase() === 'space') {
+		setRandomColors();
+	}
+});
+
 function generateRandomColor() {
 	const hexCodes = '123456789ABCDF';
 	let color = '';
