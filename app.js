5849187570bd597bc6f6697687586c87b29a405e
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-18 16:06:23 +0200

    Create generate random color func

---
+++ app.js
@@ -0,0 +1,9 @@
+function generateRandomColor() {
+	const hexCodes = '123456789ABCDF';
+	let color = '';
+	for (let i = 0; i < 6; i += 1) {
+		color += hexCodes[Math.floor(Math.random() * hexCodes.length)];	
+	}
+	return '#' + color;
+}
+
