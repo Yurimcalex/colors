19df84b523287029564a6ca8d4e8e0bed129cce3
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-18 18:35:54 +0200

    Create copy to clipboard functionality

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ document.addEventListener('click', (event) => {
 		node.classList.toggle('fa-lock-open');
 		node.classList.toggle('fa-lock');
 		document.activeElement.blur();
+	} else if (type === 'copy') {
+		copyToClickboard(event.target.textContent);
 	}
 });
 
@@ -47,6 +49,10 @@ function setRandomColors() {
 function setTextColor(text, color) {
 	const luminance = chroma(color).luminance();
 	text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
+function copyToClickboard(text) {
+	return navigator.clipboard.writeText(text);
 }
 
 setRandomColors();
