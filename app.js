6bbb1e49b7c185beea5d2700acb2cb87765b63f3
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-19 10:40:44 +0200

    Remove old get random color func

--- app.js
+++ app.js
@@ -22,15 +22,6 @@ document.addEventListener('click', (event) => {
 	}
 });
 
-function generateRandomColor() {
-	const hexCodes = '123456789ABCDF';
-	let color = '';
-	for (let i = 0; i < 6; i += 1) {
-		color += hexCodes[Math.floor(Math.random() * hexCodes.length)];	
-	}
-	return '#' + color;
-}
-
 function setRandomColors(isInitial) {
 	const colors = getColors(isInitial);
 
