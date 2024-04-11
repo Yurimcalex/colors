9e55d14ea26e1042b66ee40d4562de02402e2dfb
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-11 23:34:44 +0300

    Create LocalStorage class

--- index.js
+++ index.js
@@ -116,6 +116,45 @@ class Storage {
 }
 
 
+class LocalStorage extends Storage {
+	constructor() {
+		super();
+	}
+
+	save() {
+		let colorList = localStorage.getItem('colorList');
+		let colors = LocalStorage.getColorsFromHash();
+		let key = colors.map(color => color.slice(1)).join('');
+
+		if (colorList) {
+			colorList = JSON.parse(colorList);
+		} else {
+			colorList = {};
+		}
+
+		if (key in colorList) return [];
+
+		colorList[key] = colors;
+		localStorage.setItem('colorList', JSON.stringify(colorList));
+		return colors;
+	}
+
+	download() {
+		let colorList = localStorage.getItem('colorList');
+		if (colorList) {
+			colorList = JSON.parse(colorList);
+		} else {
+			colorList = {};
+		}
+		return colorList;
+	}
+
+	clear() {
+		localStorage.setItem('colorList', JSON.stringify({}));
+	}
+}
+
+
 class Status {
 	constructor() {
 		this.bar = document.querySelector('.status');
