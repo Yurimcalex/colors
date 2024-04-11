99560f2ae1e9ce9384210a187959b11c917f479a
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-11 19:59:43 +0300

    Create class App

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+class App {
+	constructor() {
+		this.state = {
+			colors: []
+		};
+		this.columns = new Columns();
+	}
+
+	_createColors() {
+		let colors = Storage.getColorsFromHash().map(c => new Color(c));
+		if (!colors.length) {
+			colors = [...this.columns.cols].map(() => new Color());
+		}
+		this.state.colors = colors;
+	}
+
+	_getColors() {
+		return this.state.colors.map(c => Object.values(c));
+	}
+
+	_genColors() {
+		this.state.colors.forEach(c => c.generate());
+	}
+
+	start() {
+		this._createColors();
+		this.columns.apply(this._getColors());
+	}
+
+	update() {
+		this._genColors();
+		let colors = this._getColors();
+		this.columns.apply(colors);
+		Storage.updateColorsHash(colors.map(c => c[0]));
+	}
+
+	toggleColorLock(index) {
+		this.state.colors[index].toggleLock();
+		this.columns.toggleLock(index);
+	}
+}
+
+
 class Color {
 	constructor(color = chroma.random().toString()) {
 		this._color = color;
