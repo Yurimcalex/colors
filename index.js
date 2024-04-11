ab751f0ee0eafd2bf3b0761ea9d369fbabb672a4
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-11 19:56:01 +0300

    Create class Color

---
+++ index.js
@@ -0,0 +1,22 @@
+class Color {
+	constructor(color = chroma.random().toString()) {
+		this._color = color;
+		this.luminance = chroma(color).luminance();
+		this.isLocked = false;
+	}
+
+	get color() {
+		return this._color;
+	}
+
+	generate() {
+		if (this.isLocked) return;
+		let color = chroma.random().toString();
+		this.luminance = chroma(color).luminance();
+		this._color = color;
+	}
+
+	toggleLock() {
+		this.isLocked = !this.isLocked;
+	}
+}
