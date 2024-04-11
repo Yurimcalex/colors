8d70c6c731709c4d6c9304cf2616c6aacc16282b
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-11 20:00:58 +0300

    Create class Status

--- index.js
+++ index.js
@@ -114,3 +114,16 @@ class Storage {
 			.join('-');
 	}
 }
+
+
+class Status {
+	constructor() {
+		this.bar = document.querySelector('.status');
+	}
+
+	toggle(text) {
+		this.bar.textContent = text;
+		this.bar.classList.toggle('show');
+		setTimeout(() => this.bar.classList.toggle('show'), 1000);
+	}
+}
