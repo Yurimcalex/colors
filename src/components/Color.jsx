01bae5a61983bc61639eb2bcf46544ac9a733ee0
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-12 15:52:39 +0300

    update:Color - use color prop

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Button from './Button.jsx';
 
-export default function Color() {
+export default function Color({ color }) {
 	return (
-		<div className="col">
-			<h2 data-type="copy">Text</h2>
+		<div 
+			className="col"
+			style={{
+				background: `${color}`
+			}}
+		>
+			<h2>{color}</h2>
 			<Button icon="fa-solid fa-lock-open" />
 		</div>
 	);
