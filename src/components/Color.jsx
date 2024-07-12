c28625af0d0e871f74eb38af8151752cfc8b9e41
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-12 16:42:01 +0300

    update:Color - use chroma for colors

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Button from './Button.jsx';
 
 export default function Color({ color }) {
+	const luminance = chroma(color).luminance();
+
 	return (
 		<div 
 			className="col"
 			style={{
-				background: `${color}`
+				background: color,
+				color: luminance > 0.5 ? 'black' : 'white'
 			}}
 		>
 			<h2>{color}</h2>
