9ee45df9f2cf3a945ff42e5309eff497b760732d
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-12 22:54:52 +0300

    Create Button module styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './Button.module.css';
 
 export default function Button({ icon, onClick }) {
 	return (
-		<button className="button" onClick={onClick}>
+		<button className={styles.button} onClick={onClick}>
 			<i className={icon}></i>
 		</button>
 	);
