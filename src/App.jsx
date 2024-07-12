2b060e3a468cd13ac822ca8f48aeae8119bf1d77
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-12 17:39:35 +0300

    update:App - gener colors by key press

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ColorList from './components/ColorList.jsx';
 import Storage from './storage.js';
 import './app.css';
 
 export default function App() {
-	const savedColors = Storage.getColorsFromHash();
+	const [colors, setColors] = useState(Storage.getColorsFromHash());
+
+	const handleKeyDown = (e) => {
+		const newColors = [];
+		if (e.code.toLowerCase() === 'space') {
+			for (let i = 0; i < 5; i += 1) {
+				newColors.push(chroma.random().toString());
+			}
+			setColors(newColors);
+			Storage.updateColorsHash(newColors);
+		}
+	};
+
+	useEffect(() => {
+	  window.addEventListener('keydown', handleKeyDown);
+	  return () => window.removeEventListener('keydown', handleKeyDown);
+	}, []);
 
 	return (
 		<div className="app">
-			<ColorList initialColors={savedColors} />
+			<ColorList initialColors={colors} />
 		</div>
 	);
 }
