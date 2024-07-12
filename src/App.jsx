fec896bf7e1015b78c67636ba062360df2d21da8
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-12 15:50:00 +0300

    update:App - load saved colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ColorList from './components/ColorList.jsx';
+import Storage from './storage.js';
 import './app.css';
 
 export default function App() {
+	const savedColors = Storage.getColorsFromHash();
+
 	return (
 		<div className="app">
-			<ColorList />
+			<ColorList initialColors={savedColors} />
 		</div>
 	);
 }
