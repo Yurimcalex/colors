5fa67274bfcd5ff0fb42d2ea16a985e960973f9b
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-12 18:40:38 +0300

    update:App - add color lock feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,40 @@ import './app.css';
 
 export default function App() {
 	const [colors, setColors] = useState(Storage.getColorsFromHash());
+	const [locks, setLocks] = useState(new Array(5).fill(false));
 
 	const handleKeyDown = (e) => {
 		const newColors = [];
 		if (e.code.toLowerCase() === 'space') {
 			for (let i = 0; i < 5; i += 1) {
-				newColors.push(chroma.random().toString());
+				const color = locks[i]
+					? colors[i]
+					: chroma.random().toString();
+				
+				newColors.push(color);
 			}
 			setColors(newColors);
 			Storage.updateColorsHash(newColors);
+			document.activeElement.blur();
 		}
+	};
+
+	const handleColorLock = (ind) => {
+		setLocks(locks.map((lock, i) => ind === i ? !lock : lock));
 	};
 
 	useEffect(() => {
 	  window.addEventListener('keydown', handleKeyDown);
 	  return () => window.removeEventListener('keydown', handleKeyDown);
-	}, []);
+	}, [locks]);
 
 	return (
 		<div className="app">
-			<ColorList initialColors={colors} />
+			<ColorList 
+				initialColors={colors}
+				locks={locks}
+				handleColorLock={handleColorLock}
+			/>
 		</div>
 	);
 }
