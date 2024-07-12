8fccd4152c8249941d12aec108504bd3718f06eb
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-12 15:27:25 +0300

    Create ColorList static component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import Color from './components/Color.jsx';
+import ColorList from './components/ColorList.jsx';
 import './app.css';
 
 export default function App() {
 	return (
 		<div className="app">
-			<>
-				{[1, 2, 3, 4, 5].map(n => <Color key={n} />)}
-			</>
+			<ColorList />
 		</div>
 	);
 }
