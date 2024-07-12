fa813b2da403c718edde0543a8d20c2b9a3254e7
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-12 14:34:05 +0300

    Create Color component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import Color from './components/Color.jsx';
 import './app.css';
 
 export default function App() {
 	return (
-		<div>OK</div>
+		<div>
+			<Color />
+		</div>
 	);
 }
---
+++ src/components/Color.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Color() {
+	return (
+		<div className="col">
+			<h2 data-type="copy">Text</h2>
+			<button className="button" data-type="lock">
+				<i className="fa-solid fa-lock-open"></i>
+			</button>
+		</div>
+	);
+}
