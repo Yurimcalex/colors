1915a57e6c4b76b051245bd9d2aecf38092864b3
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-13 13:26:10 +0300

    update:Button - add data-type attribute

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-export default function Button({ icon, onClick }) {
+export default function Button({ icon, onClick, dataType }) {
 	return (
-		<button className={styles.button} onClick={onClick}>
+		<button className={styles.button} onClick={onClick} data-type={dataType}>
 			<i className={icon}></i>
 		</button>
 	);
