f4d5d1bd2b35b189252d33875fdcf3e8c4e80827
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-14 22:03:39 +0300

    Create Settings component

---
+++ src/App/Settings/Settings.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../../components/Button/Button.jsx';
+import styles from './Settings.module.css';
+
+export default function Settings({ onToggleVisibility, onSaveColors, onRemoveAllSaved }) {
+	return (
+		<div className={styles.container}>
+			<Button icon="fa-solid fa-trash-can" onClick={onRemoveAllSaved} />
+			<Button icon="fa-solid fa-file-arrow-down" onClick={onToggleVisibility} />
+			<Button icon="fa-regular fa-floppy-disk" onClick={onSaveColors} />
+		</div>
+	);
+}
