a14e86405a9f3a70fc3b4f542894a139bfbbfcc5
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-15 13:09:41 +0300

    update:SavedColorsPanel - add close button

--- src/App/SavedColorsPanel/SavedColorsPanel.jsx
+++ src/App/SavedColorsPanel/SavedColorsPanel.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Button from '../../components/Button/Button.jsx';
 import styles from './SavedColorsPanel.module.css';
 
 export default function SavedColorsPanel({ children }) {
 	return (
 		<div className={styles.container}>
 			{children}
+
+			<Button 
+				icon="fa-regular fa-circle-xmark" 
+				dataType="close"
+			/>
 		</div>
 	);
 }
