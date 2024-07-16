05b8c5ad989d6c36befefeadd1ced45041d60488
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-16 18:23:14 +0300

    Create current colors ui element

--- src/features/colors/ColorsPage.jsx
+++ src/features/colors/ColorsPage.jsx
@@ -1,48 +1,11 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-	generate,
-	toggleLock, 
-	selectCurrentColors
-} from './colorsSlice.js';
-
-import Main from '../../App/Main/Main.jsx';
+import React from 'react';
 import SavedColors from './SavedColors.jsx';
+import CurrentColors from './CurrentColors.jsx';
 
 export default function ColorsPage() {
-	const { colors, locks } = useSelector(selectCurrentColors);
-	const dispatch = useDispatch();
-
-	useEffect(() => {
-	  window.addEventListener('keydown', handleKeyDown);
-	  return () => window.removeEventListener('keydown', handleKeyDown);
-	}, [locks, colors]);
-
-	const handleKeyDown = (e) => {
-		if (e.code.toLowerCase() === 'space') {
-			dispatch(generate(colors, locks));
-		}
-	};
-
-	const handleColorLock = (ind) => {
-		dispatch(toggleLock(ind));
-		document.activeElement.blur();
-	};
-
-	const handleCopyColorHash = (e) => {
-		const text = e.target.textContent;
-		navigator.clipboard.writeText(text);
-	};
-
 	return (
 		<div>
-			<Main 
-				colors={colors}
-				locks={locks}
-				handleColorLock={handleColorLock}
-				handleColorCopy={handleCopyColorHash}
-			/>
-
+			<CurrentColors />
 			<SavedColors />
 		</div>
 	);
---
+++ src/features/colors/CurrentColors.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+	generate,
+	toggleLock, 
+	selectCurrentColors
+} from './colorsSlice.js';
+
+import Main from '../../App/Main/Main.jsx';
+
+
+export default function CurrentColors() {
+	const { colors, locks } = useSelector(selectCurrentColors);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+	  window.addEventListener('keydown', handleKeyDown);
+	  return () => window.removeEventListener('keydown', handleKeyDown);
+	}, [locks, colors]);
+
+	const handleKeyDown = (e) => {
+		if (e.code.toLowerCase() === 'space') {
+			dispatch(generate(colors, locks));
+		}
+	};
+
+	const handleColorLock = (ind) => {
+		dispatch(toggleLock(ind));
+		document.activeElement.blur();
+	};
+
+	const handleCopyColorHash = (e) => {
+		const text = e.target.textContent;
+		navigator.clipboard.writeText(text);
+	};
+	
+	return (
+		<Main 
+			colors={colors}
+			locks={locks}
+			handleColorLock={handleColorLock}
+			handleColorCopy={handleCopyColorHash}
+		/>
+	);
+}
