16b684593f03e4a74f46723cb9558f8edf394306
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-16 17:30:11 +0300

    update:colorsSlice - get initial colors feature

--- src/features/colors/colorsSlice.js
+++ src/features/colors/colorsSlice.js
@@ -1,9 +1,10 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import Storage from '../../storage.js';
 
 const initialState = {
 	current: {
 		id: 1,
-		colors: ['#4682B4', '#5F9EA0', '#ff5733', '#8B4513', '#778899'],
+		colors: getInitialColors(),
 		locks: [false, false, false, false, false]
 	},
 
@@ -25,10 +26,14 @@ export const colorsSlice = createSlice({
 				state.current.colors = action.payload;
 			},
 			prepare(colors, locks) {
+				const newColors = locks.map((lock, ind) => {
+					return lock ? colors[ind] : chroma.random().toString();
+				});
+
+				Storage.updateColorsHash(newColors);
+
 				return {
-					payload: locks.map((lock, ind) => {
-						return lock ? colors[ind] : chroma.random().toString();
-					})
+					payload: newColors
 				}
 			}
 		},
@@ -47,3 +52,15 @@ export default colorsSlice.reducer;
 
 export const selectCurrentColors = (state) => state.colors.current;
 export const selectSavedColors = (state) => state.colors.saved;
+
+
+function getInitialColors() {
+	const colors = Storage.getColorsFromHash();
+	if (!colors.length) {
+		for (let i = 0; i < 5; i += 1) {
+			colors.push(chroma.random().toString());
+		}
+	}
+	Storage.updateColorsHash(colors);
+	return colors;
+}
