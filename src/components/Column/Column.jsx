75bba88ce18ac333eb51b7bc8b99f0d27aa3556c
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-17 19:06:36 +0300

    update:Column - add data-attributes to use for tooltiping

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -9,10 +9,11 @@ export default function Column({ color, lock, onColorLock, onColorCopy }) {
 	return (
 		<div className={styles.container} style={{ color: luminance > 0.5 ? 'black' : 'white' }}>
 			<Color color={color}>
-				<h2 onClick={onColorCopy}>{color}</h2>
+				<h2 onClick={onColorCopy} data-type="copy-to-clipboard">{color}</h2>
 				<Button 
 					icon={`fa-solid ${lock ? 'fa-lock' : 'fa-lock-open'}`}
-					onClick={onColorLock} 
+					onClick={onColorLock}
+					dataType="lock/unlock-color"
 				/>
 			</Color>
 		</div>
