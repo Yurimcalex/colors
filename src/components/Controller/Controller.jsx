114c50931b1d53e69b178a12a422d9a5ba42f5ab
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-18 22:55:22 +0300

    Add generate colors and save colors funcs to Controller

--- src/components/Controller/Controller.jsx
+++ src/components/Controller/Controller.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Button from '../Button/Button.jsx';
 
-export default function Controller() {
+export default function Controller({ onSaveColors, onGenerataColors }) {
 	return (
 		<div>
 			<div>
-				<Button icon="fa-solid fa-paint-roller" />
+				<Button 
+					icon="fa-solid fa-paint-roller"
+					onClick={onGenerataColors}
+				/>
 			</div>
 			
 			<div>
-				<Button icon="fa-solid fa-square-plus" />
+				<Button 
+					icon="fa-solid fa-square-plus"
+					onClick={onSaveColors}
+				/>
 			</div>
 		</div>
 	);
--- src/features/colors/ControllerColors.jsx
+++ src/features/colors/ControllerColors.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { generate, saveCurrentColors, selectCurrentColors } from './colorsSlice.js';
+
 import ControllerPanel from '../../App/ControllerPanel/ControllerPanel.jsx';
 import Controller from '../../components/Controller/Controller.jsx';
 
 export default function ControllerColors() {
+	const { colors, locks } = useSelector(selectCurrentColors);
+	const dispatch = useDispatch();
+
 	return (
 		<ControllerPanel>
-			<Controller />
+			<Controller
+				onGenerataColors={() => dispatch(generate(colors, locks))}
+				onSaveColors={() => dispatch(saveCurrentColors())}
+			/>
 		</ControllerPanel>
 	);
 }
