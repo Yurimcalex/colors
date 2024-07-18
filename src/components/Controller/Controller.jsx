7991b22e04c6dbb506e3a7f92fbe62ebde30518b
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-18 21:37:04 +0300

    Create Controller static component

---
+++ src/components/Controller/Controller.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '../Button/Button.jsx';
+
+export default function Controller() {
+	return (
+		<div>
+			<div>
+				<Button icon="fa-solid fa-paint-roller" />
+			</div>
+			
+			<div>
+				<Button icon="fa-solid fa-square-plus" />
+			</div>
+		</div>
+	);
+}
