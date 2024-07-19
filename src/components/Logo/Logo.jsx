4b09580be9df5b24d72d24e2395ed8399fd3b751
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-19 13:04:18 +0300

    Create Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Logo() {
+	return (
+		<div>
+			<span>C</span>
+			<span>o</span>
+			<span>l</span>
+			<span>o</span>
+			<span>u</span>
+			<span>r</span>
+			<span>s</span>
+		</div>
+	);
+}
