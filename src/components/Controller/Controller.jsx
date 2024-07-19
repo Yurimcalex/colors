e599c9bce76d46336216a92235bdb5e96f974067
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-19 15:46:16 +0300

    update:Controller - get an effect as a parameter

--- src/components/Controller/Controller.jsx
+++ src/components/Controller/Controller.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import Button from '../Button/Button.jsx';
 
-export default function Controller({ onSaveColors, onGenerataColors }) {
-	return (
+
+export default function Controller({ onSaveColors, onGenerataColors, Effect }) {
+
+	if (Effect) return (
 		<div>
+			<Effect>
+				<div>
+					<Button 
+						icon="fa-solid fa-paint-roller"
+						onClick={onGenerataColors}
+					/>
+				</div>
+			</Effect>
+			
+			<Effect>
+				<div>
+					<Button 
+						icon="fa-solid fa-square-plus"
+						onClick={onSaveColors}
+					/>
+				</div>
+			</Effect>
+		</div>
+	);
+
+	return (
+		<div>			
 			<div>
 				<Button 
 					icon="fa-solid fa-paint-roller"
 					onClick={onGenerataColors}
 				/>
-			</div>
-			
+			</div>	
 			<div>
 				<Button 
 					icon="fa-solid fa-square-plus"
