0ec492cf073978e91c7852c921b946a5e7131571
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-19 14:37:00 +0300

    style: Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import styles from './Logo.module.css';
 
 export default function Logo() {
 	return (
-		<div>
-			<span>C</span>
-			<span>o</span>
-			<span>l</span>
-			<span>o</span>
-			<span>u</span>
-			<span>r</span>
-			<span>s</span>
+		<div className={styles.container}>
+			<span className={styles.red}>C</span>
+			<span className={styles.orange}>o</span>
+			<span className={styles.yellow}>l</span>
+			<span className={styles.green}>o</span>
+			<span className={styles.blue}>u</span>
+			<span className={styles.purple}>r</span>
+			<span className={styles.violet}>s</span>
 		</div>
 	);
 }
