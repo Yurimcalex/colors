b8575cb84eaa283809def865b9ee6c769c039720
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-08 00:08:24 +0200

    refact: column component style

--- src/Colors/components/Column/Column.jsx
+++ src/Colors/components/Column/Column.jsx
@@ -7,8 +7,8 @@ export default function Column({ color, lock, onColorLock, onColorCopy }) {
 	const luminance = chroma(color).luminance();
 
 	return (
-		<div className={styles.container} style={{ color: luminance > 0.5 ? 'black' : 'white' }}>
-			<h2 onClick={onColorCopy} data-type="copy-to-clipboard">{color}</h2>
+		<div className={styles.column} style={{ color: luminance > 0.5 ? 'black' : 'white' }}>
+			<h2 className={styles.column_hash} onClick={onColorCopy} data-type="copy-to-clipboard">{color}</h2>
 			
 			<Button 
 				icon={`fa-solid ${lock ? 'fa-lock' : 'fa-lock-open'}`}
