a8f93b7d819ea033ec572c63b8cba9a219d0136e
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-07 23:56:31 +0200

    style: refact main content component's css

--- src/Colors/sections/MainContent/MainContent.jsx
+++ src/Colors/sections/MainContent/MainContent.jsx
@@ -4,7 +4,7 @@ import styles from './MainContent.module.css';
 
 export default function MainContent({ children }) {
 	return (
-		<div className={styles.container}>
+		<div className={styles.main_content}>
 			{children}
 		</div>
 	);
