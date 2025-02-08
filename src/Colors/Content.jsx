fc0f94000d95f48ca845dd2dfe4662d9b135cd00
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-08 19:05:37 +0200

    Create drag a color feat

--- src/Colors/Content.jsx
+++ src/Colors/Content.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { generate, toggleLock, selectCurrentColors } from '../features/colors/colorsSlice.js';
+import { generate, updateColors, toggleLock, selectCurrentColors } from '../features/colors/colorsSlice.js';
 import Color from './sections/Color/Color.jsx';
 import Column from './components/Column/Column.jsx';
 
@@ -9,10 +9,63 @@ export default function Content() {
 	const { colors, locks } = useSelector(selectCurrentColors);
 	const dispatch = useDispatch();
 
+
 	useEffect(() => {
 	  window.addEventListener('keydown', handleKeyDown);
 	  return () => window.removeEventListener('keydown', handleKeyDown);
 	}, []);
+
+	useEffect(() => {
+		window.addEventListener('mousedown', handleMouseDown);
+		window.addEventListener('mouseup', handleMouseUp);
+		window.addEventListener('mouseover', handleMouseOver);
+		window.addEventListener('mouseout', handleMouseOut);
+		return () => {
+			window.removeEventListener('mousedown', handleMouseDown);
+			window.removeEventListener('mouseup', handleMouseUp);
+			window.removeEventListener('mouseover', handleMouseOver);
+			window.removeEventListener('mouseout', handleMouseOut);
+		};
+	}, []);
+
+
+	let currentElem = null;
+	let dragStartColor;
+	const handleMouseDown = (e) => {
+		const target = e.target.closest('[data-color]');
+		if (target) {
+			dragStartColor = target.dataset.color;
+		}
+	};
+
+	const handleMouseUp = (e) => {
+		if (currentElem) {
+			const dragEndColor = currentElem.dataset.color;
+			if (dragStartColor !== dragEndColor) {
+				dispatch(updateColors(dragStartColor, dragEndColor));
+			}
+			
+		}
+	};
+
+	const handleMouseOver = (e) => {
+		if (!dragStartColor) return;
+		if (currentElem) return;
+		  let target = event.target.closest('[data-color]');
+		  if (!target) return;
+		  currentElem = target;
+	};
+
+	const handleMouseOut = (e) => {
+		if (!currentElem) return;
+		let relatedTarget = event.relatedTarget;
+		while (relatedTarget) {
+		  if (relatedTarget == currentElem) return;
+		  relatedTarget = relatedTarget.parentNode;
+		}
+		currentElem = null;
+	};
+
 
 	const handleKeyDown = (e) => {
 		if (e.code.toLowerCase() === 'space') {
@@ -30,20 +83,19 @@ export default function Content() {
 		navigator.clipboard.writeText(text);
 	};
 
-	const Colors = colors.map((color, ind) => (
-		<Color key={color} color={color}>
-			<Column 
-				color={color}
-				lock={locks[ind]}
-				onColorLock={() => handleColorLock(ind)}
-				onColorCopy={handleCopyColorHash}
-			/>
-		</Color>
-	));
 
 	return (
 		<>
-			{Colors}
+			{colors.map((color, ind) => (
+				<Color key={color} color={color}>
+					<Column 
+						color={color}
+						lock={locks[ind]}
+						onColorLock={() => handleColorLock(ind)}
+						onColorCopy={handleCopyColorHash}
+					/>
+				</Color>
+			))}
 		</>
 	);
 }
