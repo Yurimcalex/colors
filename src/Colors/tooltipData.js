2c9fe30c7ebc85825ae117f88da325687ad95be5
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-09 22:21:07 +0200

    Create tooltip data

---
+++ src/Colors/tooltipData.js
@@ -0,0 +1,71 @@
+export const tooltipData = {
+	'settings-btn-generate-colors': {
+		text: 'Generate colors',
+		place: 'bottom',
+		pointer: 'up',
+		gap: 7
+	},
+
+	'settings-btn-hide-tooltips': {
+		text: 'Hide tooltips',
+		place: 'bottom',
+		pointer: 'up',
+		gap: 7
+	},
+
+	'settings-btn-show-tooltips': {
+		text: 'Show tooltips',
+		place: 'bottom',
+		pointer: 'up',
+		gap: 7
+	},
+
+	'settings-btn-clear-store': {
+		text: 'Remove all saved colors',
+		place: 'bottom',
+		pointer: 'up',
+		gap: 7
+	},
+
+	'settings-btn-open-store': {
+		text: 'Show saved colors',
+		place: 'bottom',
+		pointer: 'up',
+		gap: 7
+	},
+
+	'settings-btn-hide-store': {
+		text: 'Hide saved colors',
+		place: 'bottom',
+		pointer: 'up',
+		gap: 7
+	},
+
+	'settings-btn-save-colors': {
+		text: 'Save current colors',
+		place: 'bottom',
+		pointer: 'up',
+		gap: 7
+	},
+
+	'column-btn-copy-hash': {
+		text: 'Copy color to clipboard',
+		place: 'bottom',
+		pointer: 'up',
+		gap: 3
+	},
+
+	'column-btn-lock-color': {
+		text: 'Lock: do not generate color',
+		place: 'bottom',
+		pointer: 'up',
+		gap: 3
+	},
+
+	'column-btn-unlock-color': {
+		text: 'Unlock: generate color',
+		place: 'bottom',
+		pointer: 'up',
+		gap: 3
+	}
+};
