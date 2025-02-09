d70dec9587ce978d337f85b5db754acfb8d4dcea
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-09 18:10:22 +0200

    Update content component to use use hover hook

--- src/Colors/Content.jsx
+++ src/Colors/Content.jsx
@@ -3,11 +3,15 @@ import { useSelector, useDispatch } from 'react-redux';
 import { generate, updateColors, toggleLock, selectCurrentColors } from '../features/colors/colorsSlice.js';
 import Color from './sections/Color/Color.jsx';
 import Column from './components/Column/Column.jsx';
+import useHover from './hooks/useHover.jsx';
 
 
 export default function Content() {
 	const { colors, locks } = useSelector(selectCurrentColors);
 	const dispatch = useDispatch();
+
+	const [hoveredElm, setHoveredElm] = useHover('[data-color]');
+	const [selectedColor, setSelectedColor] = useState('');
 
 
 	useEffect(() => {
@@ -15,56 +19,28 @@ export default function Content() {
 	  return () => window.removeEventListener('keydown', handleKeyDown);
 	}, []);
 
+
 	useEffect(() => {
 		window.addEventListener('mousedown', handleMouseDown);
 		window.addEventListener('mouseup', handleMouseUp);
-		window.addEventListener('mouseover', handleMouseOver);
-		window.addEventListener('mouseout', handleMouseOut);
 		return () => {
 			window.removeEventListener('mousedown', handleMouseDown);
 			window.removeEventListener('mouseup', handleMouseUp);
-			window.removeEventListener('mouseover', handleMouseOver);
-			window.removeEventListener('mouseout', handleMouseOut);
 		};
-	}, []);
+	}, [selectedColor, hoveredElm]);
 
-
-	let currentElem = null;
-	let dragStartColor;
 	const handleMouseDown = (e) => {
 		const target = e.target.closest('[data-color]');
-		if (target) {
-			dragStartColor = target.dataset.color;
-		}
-	};
+		if (target) setSelectedColor(target.dataset.color);
+	}
 
 	const handleMouseUp = (e) => {
-		if (currentElem) {
-			const dragEndColor = currentElem.dataset.color;
-			if (dragStartColor !== dragEndColor) {
-				dispatch(updateColors(dragStartColor, dragEndColor));
-				currentElem = null;
+		if (hoveredElm) {
+			const swapColor = hoveredElm.dataset.color;
+			if (selectedColor !== swapColor) {
+				dispatch(updateColors(selectedColor, swapColor));
 			}
-			
 		}
-	};
-
-	const handleMouseOver = (e) => {
-		if (!dragStartColor) return;
-		if (currentElem) return;
-		  let target = event.target.closest('[data-color]');
-		  if (!target) return;
-		  currentElem = target;
-	};
-
-	const handleMouseOut = (e) => {
-		if (!currentElem) return;
-		let relatedTarget = event.relatedTarget;
-		while (relatedTarget) {
-		  if (relatedTarget == currentElem) return;
-		  relatedTarget = relatedTarget.parentNode;
-		}
-		currentElem = null;
 	};
 
 
