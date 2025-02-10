a65ac229e8cc0af37f87655aca346f3b9e5192d3
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-10 18:36:13 +0200

    Update main content component to dinamically set cursor css prop

--- src/Colors/sections/MainContent/MainContent.jsx
+++ src/Colors/sections/MainContent/MainContent.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './MainContent.module.css';
 
 
-export default function MainContent({ children }) {
+export default function MainContent({ children, color }) {
 	return (
-		<div className={styles.main_content}>
+		<div className={styles.main_content} style={{ cursor: color ? 'pointer' : 'initial' }}>
 			{children}
 		</div>
 	);
