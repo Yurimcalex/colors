00b8ccd902795f379b7c881e4bc084b7d204590f
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-10 13:38:27 +0200

    Update button component to render depending on passed props

--- src/Colors/components/Button/Button.jsx
+++ src/Colors/components/Button/Button.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-export default function Button({ icon, onClick, dataType, cls }) {
+export default function Button({ icon, onClick, dataType, dataTooltip, cls }) {
+
+	if (dataType) {
+		return (
+			<button 
+				className={`${styles.button} ${cls || ''}`}
+				onClick={onClick}
+				data-type={`${dataType}`}
+			>
+				<i className={icon}></i>
+			</button>
+		);
+	}
+
+	if (dataTooltip) {
+		return (
+			<button 
+				className={`${styles.button} ${cls || ''}`}
+				onClick={onClick}
+				data-tooltip={`${dataTooltip}`}
+			>
+				<i className={icon}></i>
+			</button>
+		);
+	}
+
+	if (dataTooltip && dataType) {
+		return (
+			<button 
+				className={`${styles.button} ${cls || ''}`}
+				onClick={onClick}
+				data-type={`${dataType}`}
+				data-tooltip={`${dataTooltip}`}
+			>
+				<i className={icon}></i>
+			</button>
+		);
+	}
+
 	return (
-		<button className={`${styles.button} ${cls || ''}`} onClick={onClick} data-type={dataType}>
+		<button 
+			className={`${styles.button} ${cls || ''}`}
+			onClick={onClick}
+		>
 			<i className={icon}></i>
 		</button>
 	);
